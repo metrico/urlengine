5447a697465d27c4fa32a4e981d2efbcc0d1b836
Glitch (thin-intermediate-baryonyx) <none>
2022-06-13 17:56:06 +0000

    🏵🍦 Checkpoint ./server.js:13690106/2843

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 /** CLICKHOUSE URL Table Engine handler */
 
-const fastify = require('fastify')({ logger: true })
+const fastify = require("fastify")({ logger: true });
 var memory = []; // our fake memory storage
 
 /** CLICKHOUSE URL SELECT */
-fastify.get('/', async (request, reply) => {
+fastify.get("/", async (request, reply) => {
   return memory;
-})
+});
 
 /** CLICKHOUSE URL INSERT */
-fastify.post('/', async (request, reply) => {
-  request.body.forEach(row => memory.push({key: row.key, value: parseInt(row.value)}));
-  return {}
-})
+fastify.post("/", async (request, reply) => {
+  request.body.forEach((row) =>
+    memory.push({ key: row.key, value: parseInt(row.value) })
+  );
+  return {};
+});
 
 /**
  * @param req {FastifyRequest}
  * @returns {Promise<string>}
  */
-async function getContentBody (req) {
-  let body = ''
-  req.raw.on('data', data => {
+async function getContentBody(req) {
+  let body = "";
+  req.raw.on("data", (data) => {
     body += data.toString();
-  })
-  await new Promise(resolve => req.raw.once('end', resolve))
+  });
+  await new Promise((resolve) => req.raw.once("end", resolve));
   return body;
 }
 
@@ -31,35 +33,36 @@ async function getContentBody (req) {
  * @param req {FastifyRequest}
  * @returns {Promise<void>}
  */
-async function genericJSONParser (req) {
+async function genericJSONParser(req) {
   try {
-    var body = await getContentBody(req)
+    var body = await getContentBody(req);
     const response = body
-    .trim()
-    .split('\n')
-    .map(JSON.parse)
-    .map(obj => Object.entries(obj).sort().reduce(
-      (o, [k, v]) => (o[k] = v, o), {})
-    );
+      .trim()
+      .split("\n")
+      .map(JSON.parse)
+      .map((obj) =>
+        Object.entries(obj)
+          .sort()
+          .reduce((o, [k, v]) => ((o[k] = v), o), {})
+      );
     return response;
   } catch (err) {
-    err.statusCode = 400
-    throw err
+    err.statusCode = 400;
+    throw err;
   }
 }
 
-fastify.addContentTypeParser('*', {},
-  async function (req, body, done) {
-    return await genericJSONParser(req)
-})
+fastify.addContentTypeParser("*", {}, async function (req, body, done) {
+  return await genericJSONParser(req);
+});
 
 /** RUN URL Engine */
 const start = async () => {
   try {
-    await fastify.listen(3123)
+    await fastify.listen(3000);
   } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
-}
-start()
+};
+start();
